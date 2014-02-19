18c0dde2ee07120309649cc4ac90967e39cf2e67
Wes Ishee <wesishee@uga.edu>
2014-02-18 21:43:51 -0500

    changed join to work properly
    I hope

--- Table.java
+++ Table.java
@@ -324,11 +324,11 @@ public class Table
         	case "==":
         		// populating crossProd.  filling with only tuples where keys are equal
                 for(int i=0; i<tuples.size(); i++){
-                	for(int j=0; j<table2.attribute.length; j++){
+                	for(int j=0; j<table2.tuples.size(); j++){//prev: j<attribute.length
                 		if(tuples.get(i)[t1_colNo].equals(table2.tuples.get(j)[t2_colNo])){
                 			// creates temp array with length of cross product and copies this.table tuple into it
                         	Comparable[] temp = Arrays.copyOf(tuples.get(i), crossProd.attribute.length); 
-                        	for(int k=0; k<table2.tuples.size(); k++){ // adds in table2.tuple into temp
+                        	for(int k=0; k<table2.attribute.length; k++){ // adds in table2.tuple into temp // prev: k<tuples.size()
                         		temp[k+this.attribute.length] = table2.tuples.get(j)[k];
                         	}
                         	crossProd.insert(temp);
@@ -452,6 +452,8 @@ public class Table
             KeyType type = new KeyType(keyVal);
             if(!(this.index.containsKey(type))){ // if there is not a duplicate, add tuple into index and tuple list
             	index.put(type,tup);
+            	tuples.add(tup);
+            }else{
             	tuples.add(tup);
             }
 //          index.put (new KeyType (keyVal), tup); // this is original code
