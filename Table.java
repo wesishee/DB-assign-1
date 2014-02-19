914be788df54d06b85ba3856a72145255e69a620
Wes Ishee <wesishee@uga.edu>
2014-02-18 21:50:58 -0500

    fixed join > condition
    had it comparing < before

--- Table.java
+++ Table.java
@@ -356,7 +356,7 @@ public class Table
         	case ">":
         		for(int i=0; i<tuples.size(); i++){
                 	for(int j=0; j<table2.tuples.size(); j++){
-                		if(tuples.get(i)[t1_colNo].compareTo(table2.tuples.get(j)[t2_colNo]) < 0){
+                		if(tuples.get(i)[t1_colNo].compareTo(table2.tuples.get(j)[t2_colNo]) > 0){
                 			// creates temp array with length of cross product and copies this.table tuple into it
                         	Comparable[] temp = Arrays.copyOf(tuples.get(i), crossProd.attribute.length); 
                         	for(int k=0; k<table2.attribute.length; k++){ // adds in table2.tuple into temp
